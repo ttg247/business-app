e44d1f53a917e38652413dae646d30a9e9e2e933
ttg247 <ttggwll@gmail.com>
2023-04-23 22:55:40 +0100

    updated tree

--- resources/views/partials/head/auth.blade.php
+++ resources/views/partials/head/auth.blade.php
@@ -10,11 +10,11 @@
 
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
     
-    <link rel="stylesheet" href="../plugins/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="../plugins/ionicons/dist/css/ionicons.min.css">
-    <link rel="stylesheet" href="../plugins/icon-kit/dist/css/iconkit.min.css">
-    <link rel="stylesheet" href="../plugins/perfect-scrollbar/css/perfect-scrollbar.css">
-    <link rel="stylesheet" href="../dist/css/theme.min.css">
-    <script src="../src/js/vendor/modernizr-2.8.3.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/ionicons/dist/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/icon-kit/dist/css/iconkit.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/theme.min.css') }}">
+    <script src="{{ asset('src/js/vendor/modernizr-2.8.3.min.js') }}"></script>
 </head>
