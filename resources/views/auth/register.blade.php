13473e8f4913c4b4e2bb2379d5a28c30ecffe158
ttg247 <ttggwll@gmail.com>
2023-04-24 09:11:13 +0100

    updated auth files source

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,14 +11,14 @@
             <div class="container-fluid h-100">
                 <div class="row flex-row h-100 bg-white">
                     <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
-                        <div class="lavalite-bg" style="background-image: url('../img/auth/register-bg.jpg')">
+                        <div class="lavalite-bg" style="background-image: {{ asset('img/auth/register-bg.jpg') }}">
                             <div class="lavalite-overlay"></div>
                         </div>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-7 my-auto p-0">
                         <div class="authentication-form py-0 mx-auto">
                             <div class="logo-centered">
-                                <a href="{{ route('dashboard') }}"><img src="../src/img/brand.svg" alt=""></a>
+                                <a href="{{ route('dashboard') }}"><img src="{{ asset('src/img/brand.svg') }}" alt=""></a>
                             </div>
                             <h3>New to Bookr</h3>
                             <p>Join us today! It takes only few steps</p>
--- resources/views/partials/foot/auth.blade.php
+++ resources/views/partials/foot/auth.blade.php
@@ -1,11 +1,11 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script>window.jQuery || document.write('<script src="../src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="../plugins/popper.js/dist/umd/popper.min.js"></script>
-<script src="../plugins/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="../plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
-<script src="../plugins/screenfull/dist/screenfull.js"></script>
-<script src="../dist/js/theme.js"></script>
+<script src="{{ asset('plugins/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('plugins/screenfull/dist/screenfull.js') }}"></script>
+<script src="{{ asset('dist/js/theme.js') }}"></script>
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
